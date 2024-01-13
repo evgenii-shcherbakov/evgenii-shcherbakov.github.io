ed69520d61e99dc5dab3c7801a4bb26a7374ef14
Evgenii Shcherbakov <iipekolict@gmail.com>
2024-01-13 23:25:19 +0400

    feat: #26 revision

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { BACKEND_ENV_VALIDATOR } from '@shared/constants/env-validation';
 
+//
+
 @Module({
   imports: [
     ConfigModule.forRoot({
