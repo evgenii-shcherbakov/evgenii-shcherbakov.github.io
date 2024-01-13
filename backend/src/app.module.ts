953a91b8a0ab7347183891b45c16c56560f9c278
Evgenii Shcherbakov <iipekolict@gmail.com>
2024-01-13 22:50:45 +0400

    feat: #21 revision

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
 import { BACKEND_ENV_VALIDATOR } from '@shared/constants/env-validation';
-import { BACKEND_PUBLIC_DIR_PATH } from '@shared/constants/paths';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validationSchema: BACKEND_ENV_VALIDATOR,
-    }),
-    ServeStaticModule.forRoot({
-      rootPath: BACKEND_PUBLIC_DIR_PATH,
-      serveRoot: '/',
     }),
   ],
   controllers: [],
