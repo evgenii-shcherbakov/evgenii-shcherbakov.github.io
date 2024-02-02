245296dfa79a12b70fb49d42c2a6675147fc5ae3
Evgenii Shcherbakov <iipekolict@gmail.com>
2024-02-03 03:28:37 +0400

    feat: turborepo #28 revision

--- apps/frontend/server.ts
+++ apps/frontend/server.ts
@@ -14,7 +14,7 @@ export function app(): express.Express {
 
   const commonEngine = new CommonEngine();
 
-  // 9
+  // 10
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
