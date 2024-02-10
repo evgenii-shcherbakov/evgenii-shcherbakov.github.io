b25ef606375ee9cebd6afc1156ed46dbcf15ae05
Evgenii Shcherbakov <iipekolict@gmail.com>
2024-02-11 00:08:48 +0400

    feat: trigger backend redeploy

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from '@/app.module';
 import { corsOptions } from '@/configs/cors.config';
 import { configureSwagger } from '@/configs/swagger.config';
 
+//
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService<BackendEnvironment>);
