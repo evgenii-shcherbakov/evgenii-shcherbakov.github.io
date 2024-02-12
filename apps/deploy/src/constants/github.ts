c99640f31996543a6d7a374b515acff450487d7f
Evgenii Shcherbakov <iipekolict@gmail.com>
2024-02-13 02:54:27 +0400

    refactor: clear deploy unused code

--- apps/deploy/src/constants/github.ts
+++
@@ -1,3 +0,0 @@
-export const COMMIT_MESSAGE = process.env.GITHUB_COMMIT_MESSAGE ?? '';
-export const REPOSITORY_URL = process.env.GITHUB_REPOSITORY_URL ?? '';
-export const COMMITTER = process.env.GITHUB_COMMITTER ?? '';
--- apps/deploy/src/shared/index.ts
+++
@@ -1,5 +0,0 @@
-import { DeploymentLogger } from '@shared/logger';
-
-export const deploymentLogger = new DeploymentLogger();
-
-export * from '@shared/run-command';
