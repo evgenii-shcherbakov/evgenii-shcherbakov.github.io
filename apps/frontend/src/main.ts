b2fb84117e190142cafe37aac77b64bb1feb5777
Evgenii Shcherbakov <iipekolict@gmail.com>
2024-03-02 03:55:49 +0400

    fix: fix angular serverless function compile error #6

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -2,6 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-// 3
+// 4
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
